0ab2712ff01adda21fa318b5b01c0f7b3cfc96c9
Zac <24833352@users.noreply.github.com>
2023-11-12 21:51:09 -0500

    Updated mobile and desktop picture styling, fixed image slider

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -1,65 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
 import './ImageSlider.css';
 
-const ImageSlider = ({ slides, imageScale = 100 }) => {
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const ImageSlider = ({ slides, imageScale = 100, projectType }) => {
   const [current, setCurrent] = useState(0);
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768); // detect mobile on initial load
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    window.addEventListener('resize', handleResize); // listen for window resize
-
-    return () => {
-      window.removeEventListener('resize', handleResize); // clean up listener on component unmount
-    };
-  }, []);
-
-  const handlePrev = () => {
-    setCurrent(prev => (prev <= 0 ? slides.length - 1 : prev - 1));
+  const handleStepChange = (step) => {
+    setCurrent(step);
   };
 
-  const handleNext = () => {
-    setCurrent(prev => (prev >= slides.length - 1 ? 0 : prev + 1));
-  };
-
-  const effectiveImageScale = isMobile ? 93 : imageScale; // set scale to 100% on mobile
-
-  const imageSizeStyle = {
-    width: `${effectiveImageScale}%`,
-    height: 'auto'
-  };
-
-  const sliderSizeStyle = {
-    maxWidth: `${effectiveImageScale}%`
+  const handleClick = () => {
+    const nextIndex = (current + 1) % slides.length;
+    setCurrent(nextIndex);
   };
 
   return (
-    <div className="slider" style={sliderSizeStyle}>
-      <div className="slider-content">
-        <button 
-          className="slider-btn slider-btn-prev" 
-          onClick={handlePrev} 
-          style={{ visibility: slides.length > 1 && !isMobile ? 'visible' : 'hidden' }}
-        >
-          &lt;
-        </button>
-        <img 
-          src={slides[current].url} 
-          alt={`Slide ${current}: ${slides[current].title}`} 
-          className="slider-img" 
-          style={imageSizeStyle}
-        />
-        <button 
-          className="slider-btn slider-btn-next" 
-          onClick={handleNext} 
-          style={{ visibility: slides.length > 1 && !isMobile ? 'visible' : 'hidden' }}
-        >
-          &gt;
-        </button>
-      </div>
+    <div className="slider">
+      <AutoPlaySwipeableViews 
+        index={current}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {slides.map((slide, index) => (
+          <div key={index} onClick={handleClick} className="slider-img-container">
+            <img 
+              src={slide.url} 
+              alt={`Slide ${index}: ${slide.title}`} 
+              className={`slider-img slider-img-${projectType}`} 
+              style={{ width: `${imageScale}%`, height: 'auto' }} 
+            />
+          </div>
+
+        ))}
+      </AutoPlaySwipeableViews>
       <div className="slider-pagination">
         {slides.map((_, index) => (
           <span 
--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,44 +1,38 @@
-import React from "react"; 
+import React, { Component } from "react";
 import ImageSlider from "./ImageSlider/ImageSlider";
+import "./portfolio.css";
 
-class Portfolio extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {
-    };
-  }
+class Portfolio extends Component {
 
-  
   render() {
-    const slidesAurora = [
-      {url: 'https://i.imgur.com/jDw7fx0.png', title: 'Image 1'},
-      {url: 'https://i.imgur.com/UF6Jg3Q.png', title: 'Image 2'},
-      {url: 'https://i.imgur.com/0jxZHfH.png', title: 'Image 3'},
-      {url: 'https://i.imgur.com/6jJsVKq.png', title: 'Image 4'},
-      {url: 'https://i.imgur.com/QkhE1BK.png', title: 'Image 5'},
-      {url: 'https://i.imgur.com/8WYDU2o.jpg', title: 'Image 6'},
-    ]
-    
-    const slidesOsmosis = [
-      {url: 'https://i.imgur.com/qQHLVRi.png', title: 'Image 1'},
-      {url: 'https://i.imgur.com/14eUgQV.jpg', title: 'Image 2'},
-      {url: 'https://i.imgur.com/T2AWZr9.jpg', title: 'Image 3'},
-      {url: 'https://i.imgur.com/tkIDYWL.png', title: 'Image 4'},
-      {url: 'https://i.imgur.com/LOuPEVi.png', title: 'Image 5'},
-    ]
-
-    const slidesResellCalculator = [
-      {url: 'https://i.imgur.com/T1voMVJ.jpg', title: 'Image 1'},
-    ]
-
-    const slidesBananaMarket = [
-      {url: 'https://i.imgur.com/IGqxOyz.jpg', title: 'Image 1'},
-      {url: 'https://i.imgur.com/OigBYTF.png', title: 'Image 2'},
-      {url: 'https://i.imgur.com/si9Ke8G.jpg', title: 'Image 3'},
-      {url: 'https://i.imgur.com/MI08T0A.png', title: 'Image 4'},
-      {url: 'https://i.imgur.com/MmnFSab.jpg', title: 'Image 5'},
-    ]
+    const slides = {
+      aurora: [
+        {url: 'https://i.imgur.com/jDw7fx0.png', title: 'Image 1'},
+        {url: 'https://i.imgur.com/UF6Jg3Q.png', title: 'Image 2'},
+        {url: 'https://i.imgur.com/0jxZHfH.png', title: 'Image 3'},
+        {url: 'https://i.imgur.com/6jJsVKq.png', title: 'Image 4'},
+        {url: 'https://i.imgur.com/QkhE1BK.png', title: 'Image 5'},
+        {url: 'https://i.imgur.com/8WYDU2o.jpg', title: 'Image 6'},
+      ],
+      osmosis: [
+        {url: 'https://i.imgur.com/qQHLVRi.png', title: 'Image 1'},
+        {url: 'https://i.imgur.com/14eUgQV.jpg', title: 'Image 2'},
+        {url: 'https://i.imgur.com/T2AWZr9.jpg', title: 'Image 3'},
+        {url: 'https://i.imgur.com/tkIDYWL.png', title: 'Image 4'},
+        {url: 'https://i.imgur.com/LOuPEVi.png', title: 'Image 5'},
+      ],
+      resellCalculator: [
+        {url: 'https://i.imgur.com/T1voMVJ.jpg', title: 'Image 1'},
+      ],
+      bananaMarket: [
+        {url: 'https://i.imgur.com/IGqxOyz.jpg', title: 'Image 1'},
+        {url: 'https://i.imgur.com/OigBYTF.png', title: 'Image 2'},
+        {url: 'https://i.imgur.com/si9Ke8G.jpg', title: 'Image 3'},
+        {url: 'https://i.imgur.com/MI08T0A.png', title: 'Image 4'},
+        {url: 'https://i.imgur.com/MmnFSab.jpg', title: 'Image 5'},
+      ],
+    };
 
     const commonContainerStyles = {
       display: 'flex',
@@ -47,7 +41,7 @@ class Portfolio extends React.Component {
       width: '100%',
       margin: '0 auto',
     };
-    
+
     const mediaStyles = {
       '@media (max-width: 767px)': {
         width: '90%',
@@ -58,150 +52,165 @@ class Portfolio extends React.Component {
         height: 'auto',
       },
     };
-    
-    const containerStyles = {
-      ...commonContainerStyles,
-      height: '480px',
-      ...mediaStyles,
-    };
-    
-    const containerStylesOsmosis = {
-      ...commonContainerStyles,
-      height: '580px',
-      ...mediaStyles,
-    };
-    
-    const containerStylesRC = {
-      ...commonContainerStyles,
-      height: '680px',
-      ...mediaStyles,
-    };
-    
-    const containerStylesBM = {
-      ...commonContainerStyles,
-      height: '580px',
-      ...mediaStyles,
-    };
 
-    
-    
+    const containerStyles = type => ({
+      ...commonContainerStyles,
+      height: {
+        aurora: '480px',
+        osmosis: '580px',
+        resellCalculator: '680px',
+        bananaMarket: '580px',
+      }[type],
+      ...mediaStyles,
+    });
 
     return (
       <section id="work" className="portfolio-mf sect-pt4 route">
         <div className="container">
           <div className="row">
-              <div className="col-sm-12">
-                <div className="box-shadow-full">
-                  <div className="row">
-                    <div className="about-me pt-4 pt-md-0">
-                      <div className="title-box-2">
-                        <a href="https://github.com/zactham/AuroraProxyDashboardNoKeys" target="_blank" rel="noreferrer noopener">
-                          <h5 className="title-left">Aurora Proxies</h5>
-                        </a>
-                      </div>
-                      <div style = {containerStyles}>
-                        <ImageSlider slides={slidesAurora} imageScale={90}/>
-                      </div>
-                      
-                        <div className="work-content">
-                          <div className="row">
-                              <h2 className="w-title">Founded a business specializing in the distribution of private residential and data center proxies.  Worked in a fullstack MERN role developing a dashboard & checkout site with API calls to Stripe, Discord, MongoDB, and Oxylabs</h2>
-                              <div className="w-more">
-                                <span className="w-ctegory">
-                                ReactJS HTML Node Express
-                                </span>{" "}
-                              </div>
-                          </div>
-                        </div>
-                        
-                    </div>
-                    
-                  </div>
-                  
-                </div>
-                <div className="box-shadow-full">
-                  <div className="row">
-                    <div className="about-me pt-4 pt-md-0">
-                      <div className="title-box-2">
-                        <a href="https://twitter.com/search?q=@osmosis_app&src=typed_query" target="_blank" rel="noreferrer noopener">
-                          <h5 className="title-left">Osmosis</h5>
-                        </a>
-                      </div>
-                      <div style = {containerStylesOsmosis}>
-                        <ImageSlider slides={slidesOsmosis} imageScale={50}/>
-                      </div>
-                      
-                        <div className="work-content">
-                          <div className="row">
-                              <h2 className="w-title">Helped create an iOS paid application ($49.99) developed to purchase in-demand clothing from a limited release fashion store at high speeds.  Worked in a front end role developing customizable profiles, item keywords, live clock, google login for captcha bypass, and multiple checkout methods</h2>
-                              <div className="w-more">
-                                <span className="w-ctegory">
-                                XCode Swift JavaScript
-                                </span>{" "}
-                              </div>
-                          </div>
-                        </div>   
-                    </div>
-                  </div>
-                </div>
-
-                <div className="box-shadow-full">
-                  <div className="row">
-                    <div className="about-me pt-4 pt-md-0">
-                      <div className="title-box-2">
-                      <a href="https://appadvice.com/app/resell-calculator/1415194685" target="_blank" rel="noreferrer noopener"> 
-                        <h5 className="title-left">Resell Calculator</h5>
+            <div className="col-sm-12">
+    
+              {/* Earth 2 Section */}
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="about-me pt-4 pt-md-0">
+                    <div className="title-box-2">
+                      <a href="https://app.earth2.io/" target="_blank" rel="noreferrer noopener">
+                        <h5 className="title-left">Earth 2</h5>
                       </a>
-                      </div>
-                      <div style = {containerStylesRC}>
-                        <ImageSlider slides={slidesResellCalculator} imageScale={70}/>
-                      </div>
-                      
-                        <div className="work-content">
-                          <div className="row">
-                              <h2 className="w-title">Free IOS Application for e-commerce sellers to easily calculate their profits.  Slide the wheel to pick desired store based on marketplace's fees.</h2>
-                              <div className="w-more">
-                                <span className="w-ctegory">
-                                XCode Swift ported to React Native JS
-                                </span>{" "}
-                              </div>
-                          </div>
-                        </div>
                     </div>
-                  </div> 
-                </div>
-
-                <div className="box-shadow-full">
-                  <div className="row">
-                    <div className="about-me pt-4 pt-md-0">
-                      <div className="title-box-2">
-                      <a href="https://appadvice.com/app/banana-market/1412538188" target="_blank" rel="noreferrer noopener"> 
-                        <h5 className="title-left">Banana Market</h5>
-                      </a>
-                      </div>
-                      <div style = {containerStylesBM}>
-                        <ImageSlider slides={slidesBananaMarket} imageScale={65}/>
-
-                      </div>
-                        <div className="work-content">
-                          <div className="row">
-                              <h2 className="w-title">Free IOS Application created to teach the basic concepts of investing via a fun Stock market gaming simulator.  The ability to share scores directly to Twitter or Messages is a unique feature that allows users to compete with friends.  Contains ads through the usage of Google Firebase with the ability to pay for removal.
-</h2>
-                              <div className="w-more">
-                                <span className="w-ctegory">
-                                XCode Swift Google Firebase Ads Twitter API Integration In App Purchases
-                                </span>{" "}
-                              </div>
+                    <div className="iframe-container">
+                      <iframe
+                          src="https://www.youtube.com/embed/5oo1SSZKjrI?start=816"
+                          frameBorder="0"
+                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                          allowFullScreen
+                          title="YouTube Video"
+                      ></iframe>
+                    </div>
+                    <div className="work-content">
+                      <div className="row">
+                          <h2 className="w-title">Created 3D HDRP Unity scenes with custom lighting, raytracing, and assets for a commercial ad.  Programmed movement, UI, animation, interaction, and audio C# scripts as components of a game.</h2>
+                          <div className="w-more">
+                            <span className="w-ctegory">
+                            Unity C#
+                            </span>{" "}
                           </div>
-                        </div>  
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
+    
+              {/* Aurora Proxies Section */}
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="about-me pt-4 pt-md-0">
+                    <div className="title-box-2">
+                      <a href="https://github.com/zactham/AuroraProxyDashboardNoKeys" target="_blank" rel="noreferrer noopener">
+                        <h5 className="title-left">Aurora Proxies</h5>
+                      </a>
+                    </div>
+                    <div style={containerStyles('aurora')}>
+                      <ImageSlider slides={slides.aurora} projectType="aurora" />
+                    </div>
+                    <div className="work-content">
+                      <div className="row">
+                          <h2 className="w-title">Founded a business specializing in the distribution of private residential and data center proxies.  Worked in a fullstack MERN role developing a dashboard & checkout site with API calls to Stripe, Discord, MongoDB, and Oxylabs</h2>
+                          <div className="w-more">
+                            <span className="w-ctegory">
+                            ReactJS HTML Node Express
+                            </span>{" "}
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+    
+              {/* Osmosis Section */}
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="about-me pt-4 pt-md-0">
+                    <div className="title-box-2">
+                      <a href="https://twitter.com/search?q=@osmosis_app&src=typed_query" target="_blank" rel="noreferrer noopener">
+                        <h5 className="title-left">Osmosis</h5>
+                      </a>
+                    </div>
+                    <div style={containerStyles('osmosis')}>
+                      <ImageSlider slides={slides.osmosis} projectType="osmosis"/>
+                    </div>
+                    <div className="work-content">
+                      <div className="row">
+                        <h2 className="w-title">Helped create an iOS paid application ($49.99) developed to purchase in-demand clothing from a limited release fashion store at high speeds.  Worked in a front end role developing customizable profiles, item keywords, live clock, google login for captcha bypass, and multiple checkout methods</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                          XCode Swift JavaScript
+                          </span>{" "}
+                        </div>
+                      </div>
+                    </div>   
+                  </div>
+                </div>
+              </div>
+    
+              {/* Resell Calculator Section */}
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="about-me pt-4 pt-md-0">
+                    <div className="title-box-2">
+                      <a href="https://appadvice.com/app/resell-calculator/1415194685" target="_blank" rel="noreferrer noopener">
+                        <h5 className="title-left">Resell Calculator</h5>
+                      </a>
+                    </div>
+                    <div style={containerStyles('resellCalculator')}>
+                      <ImageSlider slides={slides.resellCalculator} projectType="resellcalc" />
+                    </div>
+                    <div className="work-content">
+                      <div className="row">
+                        <h2 className="w-title">Free IOS Application for e-commerce sellers to easily calculate their profits.  Slide the wheel to pick desired store based on marketplace's fees.</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                          XCode Swift ported to React Native JS
+                          </span>{" "}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+    
+              {/* Banana Market Section */}
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="about-me pt-4 pt-md-0">
+                    <div className="title-box-2">
+                      <a href="https://appadvice.com/app/banana-market/1412538188" target="_blank" rel="noreferrer noopener">
+                        <h5 className="title-left">Banana Market</h5>
+                      </a>
+                    </div>
+                    <div style={containerStyles('bananaMarket')}>
+                      <ImageSlider slides={slides.bananaMarket} projectType="banana"/>
+                    </div>
+                    <div className="work-content">
+                      <div className="row">
+                        <h2 className="w-title">Free IOS Application created to teach the basic concepts of investing via a fun Stock market gaming simulator.  The ability to share scores directly to Twitter or Messages is a unique feature that allows users to compete with friends.  Contains ads through the usage of Google Firebase with the ability to pay for removal.
+                        </h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                          XCode Swift Google Firebase Ads Twitter API Integration In App Purchases
+                          </span>{" "}
+                        </div>
+                      </div>
+                    </div>  
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
-    )
+    );
   }
 }
 
